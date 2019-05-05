3c6c77a2c0f27dffce710af9dc7774438c5e1bca
james <646724452@qq.com>
2019-05-05 12:11:30 +0800

    Initial Commit

---
+++ ZJOpenGLStudy/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ZJOpenGLStudy
+//
+//  Created by James on 2019/5/5.
+//  Copyright © 2019 James. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}

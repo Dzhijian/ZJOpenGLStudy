b10a43b7d239e135c672d4694503425f24f4ad11
dengzj <dengzj@canplay.com.cn>
2019-05-09 00:56:30 +0800

    03-MatrixAndShader(矩阵和着色器的使用)

--- 03-MatrixAndShader(矩阵和着色器的使用)/main.cpp
+++ 03-MatrixAndShader(矩阵和着色器的使用)/main.cpp
@@ -194,7 +194,7 @@ void SetupRC(void) {
     // 6 + 2 = 8;
     // 加载！
     //GL_TRIANGLE_FAN 以一个圆心为中心呈扇形排列，共用相邻顶点的一组三角形
-    triangleFanBatch.Begin(GL_TRIANGLES, 8);
+    triangleFanBatch.Begin(GL_TRIANGLE_FAN, 8);
     triangleFanBatch.CopyVertexData3f(vPoints);
     triangleFanBatch.End();
     
@@ -239,9 +239,6 @@ void SetupRC(void) {
     triangleStripBatch.Begin(GL_TRIANGLE_STRIP, iCounter);
     triangleStripBatch.CopyVertexData3f(vPoints);
     triangleStripBatch.End();
-
-    
-    
     
 }
 
@@ -332,26 +329,94 @@ void MouseKey(int button, int state, int x, int y){
 // 深度测试
 void DrawWireFramedBatch(GLBatch* pBatch)
 {
+    /*------------画绿色部分----------------*/
+    /* GLShaderManager 中的Uniform 值——平面着色器
+     参数1：平面着色器
+     参数2：运行为几何图形变换指定一个 4 * 4变换矩阵
+     --transformPipeline 变换管线（指定了2个矩阵堆栈）
+     参数3：颜色值
+     */
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vGreen);
+    pBatch->Draw();
+    
+    
+    
+    /*-----------边框部分-------------------*/
+    /*
+     glEnable(GLenum mode); 用于启用各种功能。功能由参数决定
+     参数列表：http://blog.csdn.net/augusdi/article/details/23747081
+     注意：glEnable() 不能写在glBegin() 和 glEnd()中间
+     GL_POLYGON_OFFSET_LINE  根据函数glPolygonOffset的设置，启用线的深度偏移
+     GL_LINE_SMOOTH          执行后，过虑线点的锯齿
+     GL_BLEND                启用颜色混合。例如实现半透明效果
+     GL_DEPTH_TEST           启用深度测试 根据坐标的远近自动隐藏被遮住的图形（材料
+     
+     
+     glDisable(GLenum mode); 用于关闭指定的功能 功能由参数决定
+     
+     */
+    
+    //画黑色边框
+    glPolygonOffset(-1.0f, -1.0f);// 偏移深度，在同一位置要绘制填充和边线，会产生z冲突，所以要偏移
+    glEnable(GL_POLYGON_OFFSET_LINE);
+    
+    // 画反锯齿，让黑边好看些
+    glEnable(GL_LINE_SMOOTH);
+    //开启混合
+    glEnable(GL_BLEND);
+    //指定混合因子
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    //绘制线框几何黑色版 三种模式，实心，边框，点，可以作用在正面，背面，或者两面
+    //通过调用glPolygonMode将多边形正面或者背面设为线框模式，实现线框渲染
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //设置线条宽度
+    glLineWidth(2.5f);
+    
+    /* GLShaderManager 中的Uniform 值——平面着色器
+     参数1：平面着色器
+     参数2：运行为几何图形变换指定一个 4 * 4变换矩阵
+     --transformPipeline.GetModelViewProjectionMatrix() 获取的
+     GetMatrix函数就可以获得矩阵堆栈顶部的值
+     参数3：颜色值（黑色）
+     */
+    
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
+    //绘制
+    pBatch->Draw();
+    
+    // 复原原本的设置
+    ////通过调用glPolygonMode将多边形正面或者背面设为全部填充模式
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    //关闭GL_POLYGON_OFFSET_LINE 模型
+    glDisable(GL_POLYGON_OFFSET_LINE);
+    //设置线条宽度
+    glLineWidth(1.0f);
+    //关闭混合
+    glDisable(GL_BLEND);
+    //关闭线条光滑功能
+    glDisable(GL_LINE_SMOOTH);
     
 }
 // 开始渲染
 void RenderScene(void)
 {
-    // 1.清除屏幕
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // 清除屏幕缓存区、深度缓冲区、模板缓存区
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     
-    // 压栈
+    //压栈
     modelViewMatrix.PushMatrix();
     M3DMatrix44f mCamera;
-    // 获取cameraFrame中的矩阵交给 mCamera
     cameraFrame.GetCameraMatrix(mCamera);
     
+    //矩阵乘以矩阵堆栈的顶部矩阵，相乘的结果随后简存储在堆栈的顶部
     modelViewMatrix.MultMatrix(mCamera);
     
-    // 获取投影矩阵
     M3DMatrix44f mObjectFrame;
-    objectFrame.GetCameraMatrix(mObjectFrame);
-
+    //只要使用 GetMatrix 函数就可以获取矩阵堆栈顶部的值，这个函数可以进行2次重载。用来使用GLShaderManager 的使用。或者是获取顶部矩阵的顶点副本数据
+    objectFrame.GetMatrix(mObjectFrame);
+    
+    //矩阵乘以矩阵堆栈的顶部矩阵，相乘的结果随后简存储在堆栈的顶部
     modelViewMatrix.MultMatrix(mObjectFrame);
     
     /* GLShaderManager 中的Uniform 值——平面着色器
@@ -361,8 +426,7 @@ void RenderScene(void)
      GetMatrix函数就可以获得矩阵堆栈顶部的值
      参数3：颜色值（黑色）
      */
-    // 通过固定管线绘图
-    shaderManager.UseStockShader(GLT_SHADER_FLAT,transformPipeline.GetModelViewProjectionMatrix(),vBlack);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), vBlack);
     
     switch(nStep) {
         case 0:
